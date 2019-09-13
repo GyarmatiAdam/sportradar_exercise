2f13646a6fba377c41ace3ae97adda95a3aa1e21
GyarmatiAdam <30920398+GyarmatiAdam@users.noreply.github.com>
2019-09-13 15:05:27 +0200

    Vue.js/Router/View->Home page added

---
+++ resources/js/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h3>Author:</h3>
+        <label>Author´s name</label>
+        <input type="text"><br>
+        <label>Date of borth</label>
+        <input type="text"><br>
+        <label>Address</label>
+        <input type="text"><br>
+        <h3>Book:</h3>
+        <label>Book´s name</label>
+        <input type="text"><br>
+        <label>Release date</label>
+        <input type="text"><br>
+        <input type="button" value="Add">
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
